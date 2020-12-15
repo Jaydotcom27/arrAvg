7f053279af11075e9e8fd3a3225cbe26f8e93034
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-12-15 17:34:50 -0400

    passed sce-1-1-2

--- test_arrayavg.py
+++ test_arrayavg.py
@@ -6,3 +6,7 @@ class testArrayavg(unittest.TestCase):
     def test_CalcAvg(self):
         result = arrayavg.CalcAvg([1, 2, 3])
         self.assertEqual(result, 2)
+
+    def test_CalcAvg2(self):
+        result = arrayavg.CalcAvg([-8, 0, 9])
+        self.assertEqual(result, 0.3333333333333333)
