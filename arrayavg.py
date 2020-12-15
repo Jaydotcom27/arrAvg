dc5dcb3d65c184b496b8989239c8c80e48ca3765
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-12-15 17:38:45 -0400

    passed sce-2-1-1, greater

--- arrayavg.py
+++ arrayavg.py
@@ -5,3 +5,12 @@ def CalcAvg(initialArray):
         sum = sum + initialArray[x]
     avg = sum/len(initialArray)
     return avg
+
+
+def GreaterVals(initialArray):
+    newArray = []
+    avg = CalcAvg(initialArray)
+    for x in range(len(initialArray)):
+        if(initialArray[x] > avg):
+            newArray.append(initialArray[x])
+    return newArray
--- test_arrayavg.py
+++ test_arrayavg.py
@@ -14,3 +14,7 @@ class testArrayavg(unittest.TestCase):
     def test_CalcAvg3(self):
         result = arrayavg.CalcAvg([-81, 90, 129, 47, 25, 11, -8, 202])
         self.assertEqual(result, 51.875)
+
+    def test_GreaterVals(self):
+        result = arrayavg.GreaterVals([1, 2, 3])
+        self.assertEqual(result, [3])
