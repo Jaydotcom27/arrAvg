4b8b21f56eac859cef6d0cfddbd31ecbe2914ee4
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-12-14 22:35:36 -0400

    passed sce-1-1-2, array sum

--- arrayavg.py
+++ arrayavg.py
@@ -1,11 +1,5 @@
-initialArray = [1, 2, 3]
-
-
 def CalcSum(initialArray):
     sum = 0
     for x in range(len(initialArray)):
         sum = sum + initialArray[x]
     return sum
-
-
-print(CalcSum(initialArray))
--- test_arrayavg.py
+++ test_arrayavg.py
@@ -6,3 +6,7 @@ class TestArrayAvg(unittest.TestCase):
     def test_sum(self):
         result = arrayavg.CalcSum([1, 2, 3])
         self.assertEqual(result, 6)
+
+    def test_sum2(self):
+        result = arrayavg.CalcSum([-4, 4, 16])
+        self.assertEqual(result, 16)
