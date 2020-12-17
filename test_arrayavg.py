8c7c9347a2b705a9ab780463654af6449c1c8f76
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-12-16 20:12:47 -0400

    passed sce-1-2-3, avg

--- test_arrayavg.py
+++ test_arrayavg.py
@@ -10,3 +10,7 @@ class TestArrayAvg(unittest.TestCase):
     def test_arrayAvg2(self):
         result = arrayavg.getGreaterThanAvg([13, 23, -51])
         self.assertEqual(result, [13, 23])
+
+    def test_arrayAvg3(self):
+        result = arrayavg.getGreaterThanAvg([13, 22, 31, 41, 53, 67, -8, 28])
+        self.assertEqual(result, [31, 41, 53, 67])
